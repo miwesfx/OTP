1149e834b7b0a3afb83dceb0c1e3debdc8952ef4
miwesfx <miwesfx@macceimar01.uca.es>
2016-04-01 11:32:51 +0200

    Initial Commit

---
+++ OTP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OTP
+//
+//  Created by Miwe sfx on 1/4/16.
+//  Copyright © 2016 Miguel Pérez Prado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

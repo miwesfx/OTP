5f203116aec0a4be2f4e4cda383a35b8d1ef8900
miwesfx <miwesfx@macceimar01.uca.es>
2016-04-01 14:37:25 +0200

    Tecnicas VC con celdas

--- OTP/TecnicasVC.swift
+++ OTP/TecnicasVC.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class TecnicasVC: UIViewController {
+class TecnicasVC: UITableViewController {
+    
+    var textArray = ["video1","video2","video3","video4"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +22,15 @@ class TecnicasVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell!
+        
+        cell.textLabel?.text = textArray[indexPath.row]
+        
+        return cell
+    }
+ 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return textArray.count
+    }
 }

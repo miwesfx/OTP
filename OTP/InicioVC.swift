50f659486b2ff2b9b663512bbf1035ceef5d7421
miwesfx <miwesfx@macceimar01.uca.es>
2016-04-15 14:18:36 +0200

    Bordes a dos botones de la pantalla principal

--- OTP/InicioVC.swift
+++ OTP/InicioVC.swift
@@ -31,9 +31,28 @@ class InicioVC: UIViewController {
         procedimientosButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
         inmovilizacionesButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
         agarresButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
-        //correoButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
-        //webButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
         
+        masterButton.layer.cornerRadius = 5
+        masterButton.layer.borderWidth = 1
+        masterButton.layer.borderColor = UIColor.darkGrayColor().CGColor
+        
+        otpButton.layer.cornerRadius = 5
+        otpButton.layer.borderWidth = 1
+        otpButton.layer.borderColor = UIColor.darkGrayColor().CGColor
+        
+        /*
+        procedimientosButton.layer.cornerRadius = 5
+        procedimientosButton.layer.borderWidth = 1
+        procedimientosButton.layer.borderColor = UIColor.darkGrayColor().CGColor
+        
+        inmovilizacionesButton.layer.cornerRadius = 5
+        inmovilizacionesButton.layer.borderWidth = 1
+        inmovilizacionesButton.layer.borderColor = UIColor.darkGrayColor().CGColor
+        
+        agarresButton.layer.cornerRadius = 5
+        agarresButton.layer.borderWidth = 1
+        agarresButton.layer.borderColor = UIColor.darkGrayColor().CGColor
+        */
     }
     
     override func didReceiveMemoryWarning() {

01ecb376e7fdf0f734237cdc2f8ccacb208b4e5b
miwesfx <Miguel@macbook-air.local>
2016-06-12 22:21:46 +0200

    traducción parcial funcional

--- OTP/OperativaVC.swift
+++ OTP/OperativaVC.swift
@@ -21,7 +21,7 @@ class OperativaVC: UIViewController {
     }
     
     override func viewDidAppear(animated: Bool) {
-        texto.text = "Operativa táctica policial.\n\nTras estudios teórico-prácticos sobre las necesidades del Policía en el ejercicio de sus funciones, en el ámbito de la profesión que desarrolla a pie de calle, en el contexto de una sociedad democrática sometida al Estado de Derecho, según directrices de Naciones Unidas con el fin de  proteger a los ciudadanos, realizados desde el enfoque de la Prevención de Riesgos Laborales y la Prevención de riesgos en general, ofrecen unos protocolos, técnicas y procedimientos de intervención policial dirigidos específicamente a la labor de las Fuerzas y Cuerpos de Seguridad, los cuales denominamos Operativa Táctica Policial\nÚNICA FORMACIÓN EN INTERVENCION POLICIAL GARANTIZADA BAJO INVESTIGACIÓN CIENTÍFICA I+D+i."
+        texto.text = NSLocalizedString("Operativa táctica policial.\n\nTras estudios teórico-prácticos sobre las necesidades del Policía en el ejercicio de sus funciones, en el ámbito de la profesión que desarrolla a pie de calle, en el contexto de una sociedad democrática sometida al Estado de Derecho, según directrices de Naciones Unidas con el fin de  proteger a los ciudadanos, realizados desde el enfoque de la Prevención de Riesgos Laborales y la Prevención de riesgos en general, ofrecen unos protocolos, técnicas y procedimientos de intervención policial dirigidos específicamente a la labor de las Fuerzas y Cuerpos de Seguridad, los cuales denominamos Operativa Táctica Policial\nÚNICA FORMACIÓN EN INTERVENCION POLICIAL GARANTIZADA BAJO INVESTIGACIÓN CIENTÍFICA I+D+i.", comment: "comentario")
         texto.textColor = UIColor.whiteColor()
         texto.font = UIFont(name: "Arial", size: 18)
         texto.textAlignment = NSTextAlignment.Justified
@@ -35,7 +35,7 @@ class OperativaVC: UIViewController {
     @IBAction func cambiarSeccion(sender: AnyObject) {
         if controller.selectedSegmentIndex == 0
             {
-            texto.text = "Operativa táctica policial.\n\nTras estudios teórico-prácticos sobre las necesidades del Policía en el ejercicio de sus funciones, en el ámbito de la profesión que desarrolla a pie de calle, en el contexto de una sociedad democrática sometida al Estado de Derecho, según directrices de Naciones Unidas con el fin de  proteger a los ciudadanos, realizados desde el enfoque de la Prevención de Riesgos Laborales y la Prevención de riesgos en general, ofrecen unos protocolos, técnicas y procedimientos de intervención policial dirigidos específicamente a la labor de las Fuerzas y Cuerpos de Seguridad, los cuales denominamos Operativa\nÚNICA FORMACIÓN EN INTERVENCION POLICIAL GARANTIZADA BAJO INVESTIGACIÓN CIENTÍFICA I+D+i."
+            texto.text = NSLocalizedString("Operativa táctica policial.\n\nTras estudios teórico-prácticos sobre las necesidades del Policía en el ejercicio de sus funciones, en el ámbito de la profesión que desarrolla a pie de calle, en el contexto de una sociedad democrática sometida al Estado de Derecho, según directrices de Naciones Unidas con el fin de  proteger a los ciudadanos, realizados desde el enfoque de la Prevención de Riesgos Laborales y la Prevención de riesgos en general, ofrecen unos protocolos, técnicas y procedimientos de intervención policial dirigidos específicamente a la labor de las Fuerzas y Cuerpos de Seguridad, los cuales denominamos Operativa\nÚNICA FORMACIÓN EN INTERVENCION POLICIAL GARANTIZADA BAJO INVESTIGACIÓN CIENTÍFICA I+D+i.", comment: "comentario")
             texto.textColor = UIColor.whiteColor()
             texto.font = UIFont(name: "Arial", size: 18)
             texto.textAlignment = NSTextAlignment.Justified
@@ -44,7 +44,7 @@ class OperativaVC: UIViewController {
             }
         if controller.selectedSegmentIndex == 1
             {
-            texto.text = "Mencionar la cobertura de validez que se la da a  OTP desde  el \"Estudio criminológico sobre prevención de riesgos en la Policía Local de Cádiz, 2003-2011\", documento elaborado  por  José Carlos Vera en base a la formación continua en OTP, que  desde el año 1997 impartido, a toda la Plantilla de la policía local de Cádiz, lo cual ha facilitado la evolución de este sistema de intervención policial, adaptado a las necesidades del policía y a la protección de los ciudadanos/as."
+            texto.text = NSLocalizedString("Mencionar la cobertura de validez que se la da a  OTP desde  el \"Estudio criminológico sobre prevención de riesgos en la Policía Local de Cádiz, 2003-2011\", documento elaborado  por  José Carlos Vera en base a la formación continua en OTP, que  desde el año 1997 impartido, a toda la Plantilla de la policía local de Cádiz, lo cual ha facilitado la evolución de este sistema de intervención policial, adaptado a las necesidades del policía y a la protección de los ciudadanos/as.", comment: "comentario")
             texto.textColor = UIColor.whiteColor()
             texto.font = UIFont(name: "Arial", size: 18)
             texto.textAlignment = NSTextAlignment.Justified
